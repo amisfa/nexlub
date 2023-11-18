abd421b52a4583fddff2d4dbaee84a5d1043232d
Sima <sima.barati1372@gmail.com>
2023-11-18 12:40:54 +0330

    sima commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Helper;
+use App\Http\Resources\Resource;
+use App\Models\Desk\Meeting\Meeting;
+use App\Notifications\System\NotificationTemplate;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,21 +19,40 @@ class AuthController extends Controller
         return view('login');
     }
 
-    public function login()
+    public function login(Request $request): JsonResponse|Resource
     {
-        return response()->json('valid', 201);
+        request()->validate([
+            'username' => 'required',
+            'password' => 'required',
+            'email' => 'required',
+            'phone_no' => 'required',
+        ]);
+
+        $user = User::where('username', $request->input('username'))->first();
+
+
+        if (!$user || !Hash::check($request->input('password'), $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.',
+            ], 401);
+        }
+
+        $plainTextToken = $user->createToken($request->input('email'))->plainTextToken;
+
+        return new Resource(['jwt' => $plainTextToken]);
     }
-//
-//    public function register()
-//    {
-//        if (auth()->id())
-//            return redirect()->route('panel.dashboard');
-//        return view('login');
-//    }
-//    public function logout()
-//    {
-//        auth()->logout();
-//        return redirect()->route('home');
-//    }
+
+
+    public function register()
+    {
+        if (auth()->id())
+            return redirect()->route('panel.dashboard');
+        return view('login');
+    }
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return redirect()->route('home');
+    }
 
 }
