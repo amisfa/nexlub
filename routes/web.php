c4c24487f24ec5f74c17e085e64b2f0027a2adfd
Sima <sima.barati1372@gmail.com>
2023-11-21 23:09:18 +0330

    sima commit

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('auth')->group(function () {
-    Route::get('/login', [AuthController::class, 'loginView']);
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/register', [AuthController::class, 'registerView']);
-    Route::get('/forget-password', [AuthController::class, 'forgetPasswordView']);
-
+    Route::get('/register', [RegisterController::class, 'create']);
+    Route::post('/register', [RegisterController::class, 'store']);
 });
