1c4da0042165188b3e2c4667da6cee51e319d193
Mohammad <erfanikia.mr@gmail.com>
2023-12-16 14:17:16 +0330

    Update dashboard.

--- resources/views/pages/Play.blade.php
+++ resources/views/pages/Play.blade.php
@@ -9,44 +9,41 @@
                 </div>
                 <div class="card-body all-icons">
                     <div class="row">
-                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6 ">
                             <a href=><img src="{{ asset('black') }}/img/1.jpg" alt="Poker">
                             </a>
                         </div>
                         <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-                            <img src="https://www.mpl.live/blog/wp-content/uploads/2022/02/Black-Jack.png" alt="BlackJack">
-                            <div class="font-icon-detail">
-                                <i class="tim-icons icon-align-center"></i>
-                                <p>icon-align-center</p>
-                            </div>
+                            <a href=><img src="{{ asset('black') }}/img/2.jpg" alt="Poker">
+                            </a>
                         </div>
-                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-                            <div class="font-icon-detail">
-                                <i class="tim-icons icon-align-left-2"></i>
-                                <p>icon-align-left-2</p>
-                            </div>
-                        </div>
-                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-                            <div class="font-icon-detail">
-                                <i class="tim-icons icon-app"></i>
-                                <p>icon-app</p>
-                            </div>
-                        </div>
-{{--                        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">--}}
-{{--                            <div class="font-icon-detail">--}}
-{{--                                <i class="tim-icons icon-atom"></i>--}}
-{{--                                <p>icon-atom</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">--}}
-{{--                            <div class="font-icon-detail">--}}
-{{--                                <i class="tim-icons icon-attach-87"></i>--}}
-{{--                                <p>icon-attach-87</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
                     </div>
+                    <div class="row">
+                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <a href=><img src="{{ asset('black') }}/img/3.jpg" alt="Poker">
+                            </a>
+                        </div>
+                        <div class="font-icon-list col-lg-5 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <a href=><img src="{{ asset('black') }}/img/4.jpg" alt="Poker">
+                            </a>
+                        </div>
+                    </div>
+
+                    {{--                        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">--}}
+                    {{--                            <div class="font-icon-detail">--}}
+                    {{--                                <i class="tim-icons icon-atom"></i>--}}
+                    {{--                                <p>icon-atom</p>--}}
+                    {{--                            </div>--}}
+                    {{--                        </div>--}}
+                    {{--                        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">--}}
+                    {{--                            <div class="font-icon-detail">--}}
+                    {{--                                <i class="tim-icons icon-attach-87"></i>--}}
+                    {{--                                <p>icon-attach-87</p>--}}
+                    {{--                            </div>--}}
+                    {{--                        </div>--}}
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
