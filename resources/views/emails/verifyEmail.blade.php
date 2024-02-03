f6a24ebec439ee4d89c7998fb806699ebf65c896
a.safa <amisfaking@gmail.com>
2024-02-03 23:49:25 +0330

    fixed verify email

--- resources/views/emails/verifyEmail.blade.php
+++ resources/views/emails/verifyEmail.blade.php
@@ -5,10 +5,10 @@
 
 Dear {{ $user->username }},
 
-Please verify your email with bellow link:
+Please verify your email by clicking on the button below:
 
 @component('mail::button', ['url' => env('URL_FRONT').'auth/email-validation?user_id='.$user->id.'&token='.$token])
-Validation
+Verify
 @endcomponent
 
 Thanks,
