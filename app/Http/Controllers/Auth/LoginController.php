7b3d5af66950d159ff89fb3339ce049f54a57fd4
a.safa <amisfaking@gmail.com>
2024-02-04 23:54:35 +0330

    test user login with id

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,7 +37,7 @@ class LoginController extends Controller
         if (!$user || !Hash::check($request->input('password'), $user->password)) {
             return redirect('auth/login')->withErrors(['password' => 'User not found or password was incorrect']);
         }
-        Auth::attempt($user);
+        Auth::guard('web')->loginUsingId($user->id);
         return redirect('/');
     }
 }
