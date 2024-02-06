15c30e97b064a446949f2a7c24357d000b4af8fe
a.safa <amisfaking@gmail.com>
2024-02-06 11:20:41 +0330

    first phase of home page

--- resources/views/pages/play.blade.php
+++ resources/views/pages/play.blade.php
@@ -21,11 +21,47 @@
             padding: 0;
             overflow: hidden;
         }
+
+        .icon-bar {
+            position: fixed;
+            bottom: 3%;
+            right: 0%;
+        }
+
+        .icon-bar a {
+            display: block;
+            text-align: center;
+            padding: 10px 12px;
+            transition: all 0.3s ease;
+            color: #bb0000;
+            font-size: 28px;
+            border-radius: 4px 0px 0px 4px;
+            box-shadow: 0px 4px 8px #000;
+        }
+
+        .home-page {
+            background: #fff;
+            color: white;
+        }
     </style>
+    <title>Play Poker</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('black') }}/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('black') }}/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('black') }}/img/favicon-16x16.png">
+    <link rel="manifest" href="{{ asset('black') }}/img/site.webmanifest">
+    <link rel="mask-icon" href="{{ asset('black') }}/img/safari-pinned-tab.svg" color="#22c9e9">
+    <meta name="msapplication-TileColor" content="#0e1726">
+    <meta name="theme-color" content="#0e1726">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
 <div>
     <iframe src='{{$url}}'></iframe>
+    <span class="icon-bar">
+            <a href="{{route('home-page')}}" target="_blank" class="home-page"><i class='bx bx-home'></i></a>
+        </span>
 </div>
 </body>
 </html>
