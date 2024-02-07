01cb9996df01580a7fe300ed569f1bf6e53197af
a.safa <amisfaking@gmail.com>
2024-02-07 23:35:56 +0330

    deleted extra comment

--- app/Http/Livewire/HeaderView.php
+++ app/Http/Livewire/HeaderView.php
@@ -6,18 +6,8 @@ use Livewire\Component;
 
 class HeaderView extends Component
 {
-//    protected $listeners = ['reloadBalance' => 'reload'];
-//
-//    public function reload(): void
-//    {
-//        $this->render();
-//    }
-
     public function render()
     {
-//        $user = Auth::user();
-//        $updatedUser = $user->fresh();
-//        Auth::setUser($updatedUser);
         return view('livewire.header-view', ['user' => auth()->user()]);
     }
 }
