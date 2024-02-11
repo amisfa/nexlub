a437eba40b19ccbd2541a7f65f0a7ad965984be7
a.safa <amisfaking@gmail.com>
2024-02-12 01:35:57 +0330

    first phase of menu tooltip

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,78 +1,86 @@
 <div class="sidebar close">
     <ul class="nav-links">
         @if(!auth()->user()->hasRole('Administrator'))
-        <li @if ($pageSlug == 'play') class="active" @endif >
-            <a href="{{route('play')}}" target="_blank">
-                <i class='bx bx-play' tooltip="Play"></i>
-                <span class="link_name">Play</span>
-            </a>
-        </li>
-        <li @if ($pageSlug == 'deposit') class="active"@endif>
-            <a href="{{route('deposit')}}">
-                <i class='bx bx-wallet'></i>
-                <span class="link_name">Deposit</span>
-            </a>
-        </li>
-        <li @if ($pageSlug == 'withdraw') class="active"@endif>
-            <a href="{{route('withdraw')}}">
-                <i class='bx bx-dollar'></i>
-                <span class="link_name">Withdraw</span>
-            </a>
-        </li>
-        <li @if ($pageSlug == 'rakeBack') class="active showMenu" @endif>
-            <div class="icon-link cursor-default">
-                <a>
-                    <i class='bx bx-trophy' style="cursor: default!important"></i>
-                    <span class="link_name">Rewards</span>
+            <li class="{{ $pageSlug == 'play' ? 'active relative' : 'relative'  }}">
+                <span class="hidden first tooltip">Play</span>
+                <a href="{{route('play')}}" target="_blank">
+                    <i class='bx bx-play'></i>
+                    <span class="link_name">Play</span>
                 </a>
-                <i class='bx bxs-chevron-down arrow'></i>
-            </div>
-            <ul class="sub-menu">
-                <li @if ($pageSlug == 'rakeBack' || $pageSlug == 'user-jack-pot' || $pageSlug == 'user-bad-beat') class="active" @endif>
-                    <a href="{{route('rake-back')}}">Rake Back</a></li>
-                <div class="flex justify-between items-center">
-                    <li @if ($pageSlug == 'user-jack-pot') class="active" @endif>
-                        <a href="{{route('jack-pot')}}">Jack Pot</a></li>
-                    @if(auth()->user()->unclaimed_jack_pot >0)
-                        <span style="background: #22c9e9"
-                              class="text-sm text-dark font-medium me-2 px-2 rounded ml-2">
+            </li>
+            <li class="{{ $pageSlug == 'deposit' ? 'active relative' : 'relative'  }}">
+                <span class="hidden tooltip">Deposit</span>
+                <a href="{{route('deposit')}}">
+                    <i class='bx bx-wallet'></i>
+                    <span class="link_name">Deposit</span>
+                </a>
+            </li>
+            <li class="{{ $pageSlug == 'withdraw' ? 'active relative' : 'relative'  }}">
+                <span class="hidden tooltip">Withdraw</span>
+                <a href="{{route('withdraw')}}">
+                    <i class='bx bx-dollar'></i>
+                    <span class="link_name">Withdraw</span>
+                </a>
+            </li>
+            <li class="{{ $pageSlug == 'rakeBack' || $pageSlug == 'user-jack-pot' || $pageSlug == 'user-bad-beat' ? 'active showMenu relative' : 'relative'  }}">
+                <span class="hidden tooltip">Rewards</span>
+                <div class="icon-link cursor-default">
+                    <a>
+                        <i class='bx bx-trophy' style="cursor: default!important"></i>
+                        <span class="link_name">Rewards</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li @if ($pageSlug == 'rakeBack') class="active" @endif>
+                        <a href="{{route('rake-back')}}">Rake Back</a></li>
+                    <div class="flex justify-between items-center">
+                        <li @if ($pageSlug == 'user-jack-pot') class="active" @endif>
+                            <a href="{{route('jack-pot')}}">Jack Pot</a></li>
+                        @if(auth()->user()->unclaimed_jack_pot >0)
+                            <span style="background: #22c9e9"
+                                  class="text-sm text-dark font-medium me-2 px-2 rounded ml-2">
                         {{auth()->user()->unclaimed_jack_pot}}
                     </span>
-                    @endif
-                </div>
-                <div class="flex justify-between items-center">
-                    <li @if ($pageSlug == 'user-bad-beat') class="active" @endif>
-                        <a href="{{route('bad-beat')}}">BadBeat</a></li>
-                    @if(auth()->user()->unclaimed_bad_beat >0)
-                        <span style="background: #22c9e9"
-                              class="text-sm text-dark font-medium me-2 px-2 rounded ml-2">
+                        @endif
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <li @if ($pageSlug == 'user-bad-beat') class="active" @endif>
+                            <a href="{{route('bad-beat')}}">BadBeat</a></li>
+                        @if(auth()->user()->unclaimed_bad_beat >0)
+                            <span style="background: #22c9e9"
+                                  class="text-sm text-dark font-medium me-2 px-2 rounded ml-2">
                         {{auth()->user()->unclaimed_bad_beat}}
                     </span>
-                    @endif
-                </div>
-            </ul>
-        </li>
-        <li @if ($pageSlug == 'referral') class="active"@endif>
-            <a href="{{route('referral')}}">
-                <i class='bx bxs-user-detail'></i>
-                <span class="link_name">Referral</span>
-            </a>
-        </li>
-            <li @if ($pageSlug == 'statics') class="active"@endif>
+                        @endif
+                    </div>
+                </ul>
+            </li>
+            <li class="{{ $pageSlug == 'referral' ? 'active relative' : 'relative'  }}">
+                <span class="hidden tooltip">Referral</span>
+                <a href="{{route('referral')}}">
+                    <i class='bx bxs-user-detail'></i>
+                    <span class="link_name">Referral</span>
+                </a>
+            </li>
+            <li class="{{ $pageSlug == 'statics' ? 'active relative' : 'relative'  }}">
+                <span class="hidden tooltip">Statics</span>
                 <a href="{{route('statics')}}">
                     <i class='bx bx-grid-alt'></i>
                     <span class="link_name">Statics</span>
                 </a>
             </li>
-            <li @if ($pageSlug == 'tickets') class="active"@endif>
-            <a href="{{route('tickets')}}">
-                <i class='bx bx-message'></i>
-                <span class="link_name">Support</span>
-            </a>
-        </li>
+            <li class="{{ $pageSlug == 'tickets' ? 'active relative' : 'relative'  }}">
+                <span class="hidden tooltip">Tickets</span>
+                <a href="{{route('tickets')}}">
+                    <i class='bx bx-message'></i>
+                    <span class="link_name">Support</span>
+                </a>
+            </li>
         @endif
-    @if(auth()->user()->hasRole('Administrator'))
-            <li @if ($pageSlug == 'user-management' || $pageSlug == 'withdraw-management' ||$pageSlug == 'ticket-management' ) class="active showMenu" @endif>
+        @if(auth()->user()->hasRole('Administrator'))
+            <li class="{{ $pageSlug == 'user-management' || $pageSlug == 'withdraw-management' ||$pageSlug == 'ticket-management' ? 'active showMenu relative' : 'relative'  }}">
+                <span class="hidden tooltip">Admin</span>
                 <div class="icon-link cursor-default">
                     <a>
                         <i class='bx bx-user' style="cursor: default!important"></i>
