0625b064a7038251636ca6e723d4e09c87ac7177
a.safa <amisfaking@gmail.com>
2024-02-11 22:59:27 +0330

    fixed action button condition

--- resources/views/vendor/laravel-views/components/actions/responsive.blade.php
+++ resources/views/vendor/laravel-views/components/actions/responsive.blade.php
@@ -1,15 +1,17 @@
 @props(['actions' => [], 'model' => null])
 
 <div>
-  @if (count($actions))
-    {{-- Mobile actions dropdown --}}
-    <div class="lg:hidden text-right">
-      <x-lv-actions.drop-down :actions="$actions" :model="$model" />
-    </div>
+    @if (count($actions) && count(array_filter($actions, function($action) use($model){
+    return $action->renderIf($model, $this) === true;
+})))
+        {{-- Mobile actions dropdown --}}
+        <div class="lg:hidden text-right">
+            <x-lv-actions.drop-down :actions="$actions" :model="$model"/>
+        </div>
 
-    {{-- Desktop action buttons --}}
-    <div class="hidden lg:flex justify-items-end">
-        <x-lv-actions.icon :actions="$actions" :model="$model" />
-    </div>
-  @endif
+        {{-- Desktop action buttons --}}
+        <div class="hidden lg:flex justify-items-end">
+            <x-lv-actions.icon :actions="$actions" :model="$model"/>
+        </div>
+    @endif
 </div>
