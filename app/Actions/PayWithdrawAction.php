1ed7b54c1aaf8a8a9b5096db108c1c3305847cc0
a.safa <amisfaking@gmail.com>
2024-02-12 23:29:59 +0330

    dd

--- app/Actions/PayWithdrawAction.php
+++ app/Actions/PayWithdrawAction.php
@@ -5,7 +5,6 @@ namespace App\Actions;
 use App\Enums\WithdrawStatuses;
 use App\Helpers\Helper;
 use App\Models\UserWithdraw;
-use Exception;
 use Illuminate\Support\Facades\Http;
 use LaravelViews\Actions\Action;
 use LaravelViews\Views\View;
@@ -33,30 +32,30 @@ class PayWithdrawAction extends Action
     public function handle($model, View $view)
     {
 //        try {
-            $withdraw = UserWithdraw::findOrFail($model->id);
-            $payAmount = 0;
-            array_map(function ($currency) use (&$payAmount, $withdraw) {
-                if ($currency['currency'] == $withdraw->currency) $payAmount = $currency['rate_usd'] * $withdraw->amount;
-            }, Helper::getAvailableCurrencies());
-            $response = Http::get('https://plisio.net/api/v1/operations/withdraw', [
-                'api_key' => env('PILISIO_SECRET_KEY'),
-                'currency' => $withdraw->currency,
-                'amount' => $payAmount,
-                'type' => 'cash_out',
-                'to' => $withdraw->user->wallet_no,
-                'feePlan' => 'normal'
-            ]);
-            if ($response->status() !== 201) $this->error('Withdraw Failed');
-            $response = json_decode($response->body(), true);
-            $withdraw->tx_url = $response['tx_url'];
-            $withdraw->status = WithdrawStatuses::Paid;
-            $withdraw->save();
-            Helper::setPokerMavens([
-                'Command' => 'LogsAddEvent',
-                'Log' => 'Paid Withdraw Id' . $withdraw->id
-            ]);
-            dd($withdraw);
-            $this->success('Withdraw Successfully');
+        $withdraw = UserWithdraw::findOrFail($model->id);
+        $payAmount = 0;
+        array_map(function ($currency) use (&$payAmount, $withdraw) {
+            if ($currency['currency'] == $withdraw->currency) $payAmount = $currency['rate_usd'] * $withdraw->amount;
+        }, Helper::getAvailableCurrencies());
+        $response = Http::get('https://plisio.net/api/v1/operations/withdraw', [
+            'api_key' => env('PILISIO_SECRET_KEY'),
+            'currency' => $withdraw->currency,
+            'amount' => $payAmount,
+            'type' => 'cash_out',
+            'to' => $withdraw->user->wallet_no,
+            'feePlan' => 'normal'
+        ]);
+        if ($response->status() !== 201) $this->error('Withdraw Failed');
+        $response = json_decode($response->body(), true);
+        $withdraw->tx_url = $response['tx_url'];
+        $withdraw->status = WithdrawStatuses::Paid;
+        $withdraw->save();
+        Helper::setPokerMavens([
+            'Command' => 'LogsAddEvent',
+            'Log' => 'Paid Withdraw Id' . $withdraw->id
+        ]);
+        dd($withdraw, $response);
+        $this->success('Withdraw Successfully');
 //        } catch (Exception $e) {
 //            $this->error('Withdraw Failed');
 //        }
