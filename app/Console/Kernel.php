602a6b0e7138658251b59b963d6697a6f3c1d8f6
a.safa <amisfaking@gmail.com>
2024-02-16 15:08:31 +0330

    add guest button to banner

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(getUserRake::class)->hourly();
+        $schedule->command(getUserRake::class)->everyFiveMinutes();
         $schedule->command(getUserStat::class)->everyFiveMinutes();
     }
 
--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -25,6 +25,20 @@
                             <br/>
                             Earn on referrals and have a great time with friends
                         </div>
+                        @guest()
+                            <br/>
+                            <div class="flex justify-center items-center">
+                                <div class="relative inline-flex  group">
+                                    <div
+                                        class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                                    </div>
+                                    <a href="{{route('signup')}}" title="Get quote now"
+                                       class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                                       role="button">Play Now
+                                    </a>
+                                </div>
+                            </div>
+                        @endguest
                     </div>
                 </div>
             </div>
