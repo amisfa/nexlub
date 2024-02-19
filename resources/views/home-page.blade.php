7c345b946361a7f728d3e084ffd37e1117f30b41
a.safa <amisfaking@gmail.com>
2024-02-19 18:20:54 +0330

    fixed banner title

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -13,9 +13,11 @@
                 style="background-color: rgba(0, 0, 0, 80%)">
                 <div class="flex h-full items-center justify-center">
                     <div class="px-2 text-center text-white md:px-12">
-                        <div class="font-bold text-left mb-1 text-3xl" style="color:#22c9e9">
+                        <div class="font-bold text-left mb-1 text-3xl border-b"
+                             style="color:#22c9e9;border-color: #22c9e9;">
                             Experience the future of <span class="banner-animation">poker with crypto!</span>
                         </div>
+                        <br/>
                         <div class="mb-8 font-bold text-left text-2xl" style="color:#22c9e9">
                             <span class="strong mr-1 banner-animation">No KYC</span>
                             <br/>
