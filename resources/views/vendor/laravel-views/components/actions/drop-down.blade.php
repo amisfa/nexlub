4cabc824b6e3f4dbdea434e151fe3b03c8e51e76
a.safa <amisfaking@gmail.com>
2024-02-23 19:00:52 +0330

    fixed profile menu

--- resources/views/vendor/laravel-views/components/actions/drop-down.blade.php
+++ resources/views/vendor/laravel-views/components/actions/drop-down.blade.php
@@ -2,7 +2,7 @@
 
 <x-lv-drop-down>
   <x-slot name="trigger">
-    <x-lv-icon-button icon="more-horizontal" size="sm"/>
+    <x-lv-icon-button icon="align-right" size="sm"/>
   </x-slot>
   <x-lv-actions.icon-and-title :actions="$actions" :model="$model" />
 </x-lv-drop-down>
