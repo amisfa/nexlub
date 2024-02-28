22bb602887ae368e017141ac3ed3d061de3912ee
a.safa <amisfaking@gmail.com>
2024-02-28 22:54:58 +0330

    fixed mavens data logs

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -23,81 +23,81 @@ class Helper
             $handName = request('Hand');
             $tableName = request('Name');
             $logs = Helper::getHistoryLog($handName, $tableName);
-        }
-        if (count($logs)) {
-            foreach ($logs['ring_game'] as $username => $stat) {
-                $user = User::query()->where('username', $username)->first();
-                if ($user) {
-                    if ($stat['jack_pot_amount'] > 0) {
-                        UserJackPotReward::query()->create([
-                            'user_id' => $user->id,
-                            'amount' => $stat['jack_pot_amount']
-                        ]);
-                    }
-                    if ($stat['bad_beat_amount']) {
-                        UserBadBeatReward::query()->create([
-                            'user_id' => $user->id,
-                            'amount' => $stat['bad_beat_amount']
-                        ]);
-                    }
-                    if ($user->ringGameStats()->exists()) {
-                        $userCashGameStat = UserRingGameStat::query()->where('user_id', $user->id);
-                        $currentCashGameStat = $userCashGameStat->first();
-                        $userCashGameStat->update(
-                            [
-                                "total_net" => $currentCashGameStat->total_net + $stat['total_net'],
-                                "hand_count" => $currentCashGameStat->hand_count + $stat['hand_count'],
-                                "win_count" => $currentCashGameStat->win_count + $stat['win_count'],
-                                "lose_count" => $currentCashGameStat->lose_count + $stat['lose_count'],
-                                "folded_on_preflop_count" => $currentCashGameStat->folded_on_preflop_count + $stat['folded_on_preflop_count'],
-                                "won_without_showdown_count" => $currentCashGameStat->won_without_showdown_count + $stat['won_without_showdown_count'],
-                                "showdown_count" => $currentCashGameStat->showdown_count + $stat['showdown_count'],
-                                "folded_on_river_count" => $currentCashGameStat->folded_on_river_count + $stat['folded_on_river_count'],
-                                "folded_on_flop_count" => $currentCashGameStat->folded_on_flop_count + $stat['folded_on_flop_count'],
-                                "folded_on_turn_count" => $currentCashGameStat->folded_on_turn_count + $stat['folded_on_turn_count'],
-                                "total_jack_pot_amount" => $currentCashGameStat->total_jack_pot_amount + $stat['jack_pot_amount'],
-                                "total_bad_beat_amount" => $currentCashGameStat->total_bad_beat_amount + $stat['bad_beat_amount'],
-
+            if (count($logs)) {
+                foreach ($logs['ring_game'] as $username => $stat) {
+                    $user = User::query()->where('username', $username)->first();
+                    if ($user) {
+                        if ($stat['jack_pot_amount'] > 0) {
+                            UserJackPotReward::query()->create([
+                                'user_id' => $user->id,
+                                'amount' => $stat['jack_pot_amount']
                             ]);
-                    } else {
-                        UserRingGameStat::query()->create([
-                            'user_id' => $user->id,
-                            "total_net" => $stat['total_net'],
-                            "hand_count" => $stat['hand_count'],
-                            "win_count" => $stat['win_count'],
-                            "lose_count" => $stat['lose_count'],
-                            "folded_on_preflop_count" => $stat['folded_on_preflop_count'],
-                            "won_without_showdown_count" => $stat['won_without_showdown_count'],
-                            "showdown_count" => $stat['showdown_count'],
-                            "folded_on_river_count" => $stat['folded_on_river_count'],
-                            "folded_on_flop_count" => $stat['folded_on_flop_count'],
-                            "folded_on_turn_count" => $stat['folded_on_turn_count'],
-                            "total_jack_pot_amount" => $stat['jack_pot_amount'],
-                            "total_bad_beat_amount" => $stat['bad_beat_amount'],
-                        ]);
+                        }
+                        if ($stat['bad_beat_amount']) {
+                            UserBadBeatReward::query()->create([
+                                'user_id' => $user->id,
+                                'amount' => $stat['bad_beat_amount']
+                            ]);
+                        }
+                        if ($user->ringGameStats()->exists()) {
+                            $userCashGameStat = UserRingGameStat::query()->where('user_id', $user->id);
+                            $currentCashGameStat = $userCashGameStat->first();
+                            $userCashGameStat->update(
+                                [
+                                    "total_net" => $currentCashGameStat->total_net + $stat['total_net'],
+                                    "hand_count" => $currentCashGameStat->hand_count + $stat['hand_count'],
+                                    "win_count" => $currentCashGameStat->win_count + $stat['win_count'],
+                                    "lose_count" => $currentCashGameStat->lose_count + $stat['lose_count'],
+                                    "folded_on_preflop_count" => $currentCashGameStat->folded_on_preflop_count + $stat['folded_on_preflop_count'],
+                                    "won_without_showdown_count" => $currentCashGameStat->won_without_showdown_count + $stat['won_without_showdown_count'],
+                                    "showdown_count" => $currentCashGameStat->showdown_count + $stat['showdown_count'],
+                                    "folded_on_river_count" => $currentCashGameStat->folded_on_river_count + $stat['folded_on_river_count'],
+                                    "folded_on_flop_count" => $currentCashGameStat->folded_on_flop_count + $stat['folded_on_flop_count'],
+                                    "folded_on_turn_count" => $currentCashGameStat->folded_on_turn_count + $stat['folded_on_turn_count'],
+                                    "total_jack_pot_amount" => $currentCashGameStat->total_jack_pot_amount + $stat['jack_pot_amount'],
+                                    "total_bad_beat_amount" => $currentCashGameStat->total_bad_beat_amount + $stat['bad_beat_amount'],
+
+                                ]);
+                        } else {
+                            UserRingGameStat::query()->create([
+                                'user_id' => $user->id,
+                                "total_net" => $stat['total_net'],
+                                "hand_count" => $stat['hand_count'],
+                                "win_count" => $stat['win_count'],
+                                "lose_count" => $stat['lose_count'],
+                                "folded_on_preflop_count" => $stat['folded_on_preflop_count'],
+                                "won_without_showdown_count" => $stat['won_without_showdown_count'],
+                                "showdown_count" => $stat['showdown_count'],
+                                "folded_on_river_count" => $stat['folded_on_river_count'],
+                                "folded_on_flop_count" => $stat['folded_on_flop_count'],
+                                "folded_on_turn_count" => $stat['folded_on_turn_count'],
+                                "total_jack_pot_amount" => $stat['jack_pot_amount'],
+                                "total_bad_beat_amount" => $stat['bad_beat_amount'],
+                            ]);
+                        }
                     }
                 }
-            }
-            foreach ($logs['sng'] as $username => $stat) {
-                $user = User::query()->where('username', $username)->first();
-                if ($user) {
-                    if ($user->sngStats()->exists()) {
-                        $userSngStat = UserSNGStat::query()->where('user_id', $user->id);
-                        $currentSngStat = $userSngStat->first();
-                        $userSngStat->update([
-                            [
-                                "win_count" => $currentSngStat->win_count + $stat['win_count'],
-                                "lose_count" => $currentSngStat->lose_count + $stat['lose_count'],
-                                "net_chip" => $currentSngStat->total_net_chip + $stat['net_chip'],
-                            ]
-                        ]);
-                    } else {
-                        UserSNGStat::query()->create([
-                            'user_id' => $user->id,
-                            "win_count" => $stat['win_count'],
-                            "lose_count" => $stat['lose_count'],
-                            "net_chip" => $stat['net_chip'],
-                        ]);
+                foreach ($logs['sng'] as $username => $stat) {
+                    $user = User::query()->where('username', $username)->first();
+                    if ($user) {
+                        if ($user->sngStats()->exists()) {
+                            $userSngStat = UserSNGStat::query()->where('user_id', $user->id);
+                            $currentSngStat = $userSngStat->first();
+                            $userSngStat->update([
+                                [
+                                    "win_count" => $currentSngStat->win_count + $stat['win_count'],
+                                    "lose_count" => $currentSngStat->lose_count + $stat['lose_count'],
+                                    "net_chip" => $currentSngStat->total_net_chip + $stat['net_chip'],
+                                ]
+                            ]);
+                        } else {
+                            UserSNGStat::query()->create([
+                                'user_id' => $user->id,
+                                "win_count" => $stat['win_count'],
+                                "lose_count" => $stat['lose_count'],
+                                "net_chip" => $stat['net_chip'],
+                            ]);
+                        }
                     }
                 }
             }
